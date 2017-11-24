c865ca83594fc67f6da445d6408176e47eae2428
王楠 <773013686@qq.com>
2017-11-24 23:54:45 +0800

    add: touch event listener

--- experimental/task12/js/TodoMVC.js
+++ experimental/task12/js/TodoMVC.js
@@ -35,6 +35,15 @@ var TodoMVC;
                                 '<i class="{progress} {priority}"></i>',
                             '</div>',
                             '<div class="content-paragraph">{content}</div>',
+                            '<div class="content-operation content-operation-left">',
+                                '<div class="content-button">编辑</div>',
+                                '<div class="content-button">删除</div>',
+                            '</div>',
+                            '<div class="content-operation content-operation-right">',
+                                '<div class="content-button">已完成</div>',
+                                '<div class="content-button">待办</div>',
+                                '<div class="content-button">进行中</div>',
+                            '</div>',
                         '</div>'
                     ].join('');
                 } (),
@@ -71,6 +80,103 @@ var TodoMVC;
                     taskListTpl += taskTpl;
                 }
                 container.innerHTML = taskListTpl;
+                V.addEvent();
+            },
+            addEvent: function () {
+                var items = document.querySelectorAll('.content-item');
+                items.forEach(function (item, idx) {
+                    V.touchEvent.addTo(item);
+                })
+            },
+            touchEvent: {
+                data: {
+                    startX: 0,
+                    startY: 0,
+                    startTranslateX: 0,
+                    startTranslateY: 0
+                },
+                touchstart: function (e) {
+                    e.preventDefault();
+                    console.log('touch start', e);
+
+                    // 获取接触屏幕时的X和Y
+                    V.touchEvent.data.startX = e.changedTouches[0].pageX;
+                    V.touchEvent.data.startY = e.changedTouches[0].pageY;
+
+                    // 获取接触屏幕时operation的translate值
+                    var operationLeft = e.target.querySelector('.content-operation-left');
+                    operationLeft.style.display = 'block';
+                    var operationRight = e.target.querySelector('.content-operation-right');
+                    operationRight.style.display = 'block';
+
+                    // var style = getComputedStyle(operation);
+                    // var transform = style.transform;
+                    // // transform: matrix(1, 0, 0, 1, -128, 0);
+                    // var match = transform.match(/matrix\(1, 0, 0, 1, (-?[\d.]*), (-?[\d.]*)/);
+                    // if (match) {
+                    //     V.touchEvent.data.startTranslateX = parseInt(match[1]);
+                    //     V.touchEvent.data.startTranslateY = parseInt(match[2]);
+                    // }
+                    e.target.addEventListener('touchmove', V.touchEvent.touchmove);
+                    e.target.addEventListener('touchend', V.touchEvent.touchend);
+                },
+                touchmove: function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    console.log('touch move');
+                    // 获取滑动屏幕时的X,Y
+                    var endX = e.changedTouches[0].pageX;
+                    var endY = e.changedTouches[0].pageY;
+                    //获取滑动距离
+                    var distanceX = endX - V.touchEvent.data.startX;
+                    var distanceY = endY - V.touchEvent.data.startY;
+                    if (distanceX >= 0) {
+                        var operation = e.target.querySelector('.content-operation-left');
+                        // operation.style.display = 'block';
+                        var width = parseInt(getComputedStyle(operation).width);
+                        distanceX = distanceX < width ? distanceX : width;
+                        operation.style.transform = 'translateX(' + (distanceX - width) + 'px)';
+                    } else {
+                        var operation = e.target.querySelector('.content-operation-right');
+                        // operation.style.display = 'block';
+                        var width = parseInt(getComputedStyle(operation).width);
+                        console.log(distanceX, width);
+                        distanceX = -distanceX < width ? -distanceX : width;
+                        operation.style.transform = 'translateX(' + (width - distanceX) + 'px)';
+                    }
+                    // console.log(distanceX, distanceY);
+                },
+                touchend: function (e) {
+                    e.preventDefault();
+                    console.log('touch end', e);
+                    // 获取滑动屏幕时的X,Y
+                    var endX = e.changedTouches[0].pageX;
+                    var endY = e.changedTouches[0].pageY;
+                    //获取滑动距离
+                    var distanceX = endX - V.touchEvent.data.startX;
+                    var distanceY = endY - V.touchEvent.data.startY;
+                    //滑动方向
+                    if (Math.abs(distanceX) > Math.abs(distanceY) && distanceX > 50) {
+                        console.log('往右滑动');
+                    } else if (Math.abs(distanceX) > Math.abs(distanceY) && distanceX < -50) {
+                        console.log('往左滑动');
+                    } else if (Math.abs(distanceX) < Math.abs(distanceY) && distanceY < -50) {
+                        console.log('往上滑动');
+                    } else if (Math.abs(distanceX) < Math.abs(distanceY) && distanceY > 50) {
+                        console.log('往下滑动');
+                    }
+                    // 
+                    var operationLeft = e.target.querySelector('.content-operation-left');
+                    // operationLeft.style.display = 'none';
+                    var operationRight = e.target.querySelector('.content-operation-right');
+                    // operationRight.style.display = 'none';
+
+                    e.target.removeEventListener('touchmove', V.touchEvent.touchmove);
+                    e.target.removeEventListener('touchend', V.touchEvent.touchend);
+                },
+                addTo: function (dom) {
+                    dom.addEventListener('touchstart', V.touchEvent.touchstart);
+                }
             }
         };
         return function (v) {
